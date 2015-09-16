2cb6a99fe3a631f13c430686a6a46630a33a1c56
scruffmuffler <sloan.coffin@gmail.com>
2015-09-15 18:26:38 -0700

    file upload to s3 is working

--- assets/js/controllers/AddSongCtrl.js
+++ assets/js/controllers/AddSongCtrl.js
@@ -4,23 +4,53 @@ BandStormApp.controller('AddSongCtrl', ['$scope','$http','$mdDialog','$routePara
 
 
   $scope.addSong = function(){
-    console.log($scope.newSong.file);
-    $http.post('/api/song/create',{
-      name: $scope.newSong.name,
-      description:$scope.newSong.description,
-      version:$scope.newSong.version,
-      project: $routeParams.id,
-      song:$scope.song
-    }).success(function(data){
-      console.log('this - ',data)
-    })
-  }
+        var fd = new FormData();
 
+        //you can also send other fields
+        //this will be available as req.body.title
+        //NOTE: files must be added AFTER other form data
+        fd.append('name', $scope.titleText);
+        //NOTE: files must be added AFTER other form data
+        //this will be available as req.body.description
+        fd.append('description', $scope.descriptionText);
+        //this will be available as req.body.description
+        fd.append('version', $scope.versionText);
+        //this will be available as req.project;
+        fd.append('project', $routeParams.id);
+
+        //song relates to what we called the file
+        //in the api on sails
+        fd.append('song', $scope.uploadFile);
+
+
+
+        $http.post('/api/song/create', fd, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        }).success(function(data){
+          console.log('this - ',data)
+        }).error(function(err){
+            alert('there was an error uploading the file.');
+            console.log(err);
+        });
+      }
+  //   $http.post('/api/song/create',{
+  //     name: $scope.newSong.name,
+  //     description:$scope.newSong.description,
+  //     version:$scope.newSong.version,
+  //     project: $routeParams.id,
+  //     song:$scope.song
+  //   }).success(function(data){
+  //     console.log('this - ',data)
+  //   })
+  // }
   $scope.closeModal = function() {
       $mdDialog.hide();
   }
 
 }]);
+
+
 
 
 
